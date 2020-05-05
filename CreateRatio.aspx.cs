84eea88f32e718ad1799819d775f6dcb9561b996
Zaid Vakil <zaidvakil27@gmail.com>
2020-05-04 22:25:25 -0700

    numerator/denominator textboxes no longer convert to scientific notation

--- CreateRatio.aspx.cs
+++ CreateRatio.aspx.cs
@@ -7,6 +7,8 @@ using System.Web.UI.WebControls;
 using System.Data.OleDb;
 using System.Configuration;
 using System.Text.RegularExpressions;
+using System.Threading;
+using System.Text;
 
 namespace Corporate_Performance_Analyzer
 {
@@ -45,7 +47,7 @@ namespace Corporate_Performance_Analyzer
             
 
             num_array.Add(constructFloat);
-            List<string> l2 = num_array.ConvertAll<string>(x => x.ToString());
+            List<string> l2 = num_array.ConvertAll<string>(x => ToLongString(x));
             foreach (string s in l2)
             {
                 numerator_textBox.Text = numerator_textBox.Text + " + " + s;
@@ -65,10 +67,10 @@ namespace Corporate_Performance_Analyzer
 
 
             den_array.Add(constructFloat);
-            List<string> l2 = den_array.ConvertAll<string>(x => x.ToString());
+            List<string> l2 = den_array.ConvertAll<string>(x => x.ToString("F15").TrimEnd('0'));
             foreach (string s in l2)
             {
-                denominator_textBox.Text = denominator_textBox.Text + " + " + s;
+                denominator_textBox.Text = denominator_textBox.Text + "+" + s;
             }
         }
 
@@ -81,19 +83,76 @@ namespace Corporate_Performance_Analyzer
         {
 
         }
-        private void showRatio()
+
+
+
+
+
+
+        private static string ToLongString(double input)
         {
-            String connString = System.Configuration.ConfigurationManager.ConnectionStrings["CPADataConnectionString"].ToString();
-            conn = new System.Data.OleDb.OleDbConnection(connString);
-            conn.Open();
-            queryStr = "";
+            string strOrig = input.ToString();
+            string str = strOrig.ToUpper();
 
-            cmd = new System.Data.OleDb.OleDbCommand(queryStr, conn);
+            // if string representation was collapsed from scientific notation, just return it:
+            if (!str.Contains("E")) return strOrig;
 
-            cmd.ExecuteReader();
+            bool negativeNumber = false;
 
-            conn.Close();
+            if (str[0] == '-')
+            {
+                str = str.Remove(0, 1);
+                negativeNumber = true;
+            }
 
+            string sep = Thread.CurrentThread.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+            char decSeparator = sep.ToCharArray()[0];
+
+            string[] exponentParts = str.Split('E');
+            string[] decimalParts = exponentParts[0].Split(decSeparator);
+
+            // fix missing decimal point:
+            if (decimalParts.Length == 1) decimalParts = new string[] { exponentParts[0], "0" };
+
+            int exponentValue = int.Parse(exponentParts[1]);
+
+            string newNumber = decimalParts[0] + decimalParts[1];
+
+            string result;
+
+            if (exponentValue > 0)
+            {
+                result =
+                    newNumber +
+                    GetZeros(exponentValue - decimalParts[1].Length);
+            }
+            else // negative exponent
+            {
+                result =
+                    "0" +
+                    decSeparator +
+                    GetZeros(exponentValue + decimalParts[0].Length) +
+                    newNumber;
+
+                result = result.TrimEnd('0');
+            }
+
+            if (negativeNumber)
+                result = "-" + result;
+
+            return result;
+        }
+
+        private static string GetZeros(int zeroCount)
+        {
+            if (zeroCount < 0)
+                zeroCount = Math.Abs(zeroCount);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < zeroCount; i++) sb.Append("0");
+
+            return sb.ToString();
         }
     }
 }
